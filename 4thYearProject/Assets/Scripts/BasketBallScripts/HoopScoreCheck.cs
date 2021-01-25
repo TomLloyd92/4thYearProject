ee799e4cc09e114b4acd3d6a7a30cbd33714e294
TomLloyd92 <c00231308@itcarlow.ie>
2021-01-25 16:25:06 +0000

    Base Basket Ball mini game in

---
+++ 4thYearProject/Assets/Scripts/BasketBallScripts/HoopScoreCheck.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoopScoreCheck : MonoBehaviour
+{
+    //Ticket
+
+    //Audio Clips for Basket
+   // public AudioSource sound1;
+   // public AudioSource sound2;
+    //public AudioSource sound3;
+
+    //Particle Transform for Basket
+    public Transform fireworks;
+
+    public ParticleSystem fireworkss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // fireworks.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if(collider.tag == "BasketBall" )
+        {
+            //fireworks.GetComponent<ParticleSystem>().emission.enabled;
+            Debug.Log("BASKET");
+
+            fireworkss.Play();
+            StartCoroutine(stopFireWorks());
+
+          //  sound1.Play();
+          //  sound2.Play();
+           // sound3.Play();
+        }
+
+        //Instantiate(ticket);
+    }
+
+    IEnumerator stopFireWorks()
+    {
+        yield return new WaitForSeconds(3);
+        fireworkss.Stop();
+    }
+
+}
