ebd7568368f7b62dd3f3b4009016beb2db714560
TomLloyd92 <c00231308@itcarlow.ie>
2021-02-13 10:55:17 +0000

    Base ticket in

---
+++ 4thYearProject/Assets/Scripts/KanBan/Backlog.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backlog : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Ticket")
+        {
+            collider.GetComponent<Ticket>().freezeTicket();
+
+            Debug.Log("YEHAW");
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Ticket")
+        {
+            collider.GetComponent<Ticket>().unfreezeTicket();
+
+            //collider.attachedRigidbody.useGravity = true;
+            //ticket.useGravity = true;
+            Debug.Log("OH NO");
+        }
+    }
+
+}
---
+++ 4thYearProject/Assets/Scripts/KanBan/Ticket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ticket : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void freezeTicket()
+    {
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
+    }
+
+    public void unfreezeTicket()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+    }
+
+}
